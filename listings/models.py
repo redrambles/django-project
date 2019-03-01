9404bde705cf715754319554b09f409b4498764a
Ann Cascarano <a.cascarano@gmail.com>
2019-03-01 13:37:44 -0500

    Add models and migrations for listings and realtors tables

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
 
-# Create your models here.
+# Don't need an id field - that is automatic
+class Listing(models.Model):
+  realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+  title = models.CharField(max_length=200)
+  address = models.CharField(max_length=200)
+  city = models.CharField(max_length=100)
+  state = models.CharField(max_length=100)
+  zipcode = models.CharField(max_length=20)
+  description = models.TextField(blank=True) #This field is optional
+  price = models.IntegerField()
+  bedroooms = models.IntegerField()
+  bathrooms = models.DecimalField(max_digits=2, decimal_places=1) # Ex: 1.5
+  garage = models.IntegerField(default=0)
+  sqft = models.IntegerField()
+  lot_size = models.DecimalField(max_digits=5, decimal_places=1) # Ex: 2.4 acres
+  photo_main = models.ImageField(upload_to='photos/%Y/%M/%d/')
+  photo_1 = models.ImageField(upload_to='photos/%Y/%M/%d/', blank=True)
+  photo_2 = models.ImageField(upload_to='photos/%Y/%M/%d/', blank=True)
+  photo_3 = models.ImageField(upload_to='photos/%Y/%M/%d/', blank=True)
+  photo_4 = models.ImageField(upload_to='photos/%Y/%M/%d/', blank=True)
+  photo_5 = models.ImageField(upload_to='photos/%Y/%M/%d/', blank=True)
+  photo_6 = models.ImageField(upload_to='photos/%Y/%M/%d/', blank=True)
+  is_published = models.BooleanField(default=True)
+  list_date = models.DateTimeField(default=datetime.now, blank=True)
+  def __str__(self):
+    return self.title # This is to set the main field for the admin
