26e985ef7bf839dcf28ea154fa408fe02b4abdee
Ann Cascarano <a.cascarano@gmail.com>
2019-03-03 14:31:20 -0500

    Homepage and about page are dynamic - working on single listing

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
 
 # You've already specified to look for templates in the 'templates' folder, inside the settings.py file, so the path is in relation to that
 
 # Create your views here.
 def index(request):
-  return render(request, 'pages/index.html')
+  listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+  context = {
+    'listings': listings
+  }
+
+  return render(request, 'pages/index.html', context)
 
 def about(request):
-  return render(request, 'pages/about.html')
+  realtors = Realtor.objects.order_by('-hire_date')
+
+  # Get MVP
+  mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+  context = {
+    'realtors': realtors,
+    'mvp_realtors': mvp_realtors
+  }
+
+  return render(request, 'pages/about.html', context)
